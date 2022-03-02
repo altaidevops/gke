d143ff3dfeb0db27e6821242d8e10560d30e932f
altaidevops <88740731+altaidevops@users.noreply.github.com>
2022-03-01 20:24:21 -0600

    Add files via upload

---
+++ 7.jenkins-setup/envs/dev.tfvars
@@ -0,0 +1,4 @@
+google_domain_name = "projectxconsulting.net"
+jenkins_username = "admin"
+jenkins_password = "strongpassword"
+deployment_name = "jenkins"
---
+++ 7.jenkins-setup/variables.tf
@@ -0,0 +1,7 @@
+variable "deployment_name" {
+  type        = string
+  description = "- (Required) Chart release name."
+}
+variable "google_domain_name" {}
+variable "jenkins_password" {}
+variable "jenkins_username" {}
